116ed6b78721e0173c300b40c900f8fed9d365e3
Debug <xx.sxdev01@gmail.com>
2026-01-05 14:15:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 
 termux-setup-storage
 
+# Cài đặt các gói hỗ trợ hệ thống
 pkg install -y git python python-pip openjdk-17
 
+# Cài đặt các thư viện Python cần thiết
 pip install gdown licensing mysql-connector-python requests
 
-git clone https://github.com/xxsxdev01-debug/NgocRongTerrmux.git
+# Tải mã nguồn từ Repository của bạn (Sửa từ JINN1368 sang xxsxdev01-debug)
+git clone https://github.com/xxsxdev01-debug/DragonBall
+
 clear
-cd NgocRongTermux 
+
+# Di chuyển vào thư mục DragonBall
+cd DragonBall 
+
+# Đưa các file khởi động vào hệ thống Termux
 mv *.sh ~/../usr/bin/
 chmod +x ~/../usr/bin/*.sh
+
+# Quay lại thư mục gốc và khởi chạy lệnh của bạn
 cd
 clear
 debug.sh
