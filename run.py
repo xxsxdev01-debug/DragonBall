7fbb31f4161e991d8028a188909a03d4a5fc23d4
Debug <xx.sxdev01@gmail.com>
2026-01-05 15:01:19 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,18 @@
 import requests
 
-# 1. Link kiểm tra ID (Nếu muốn)
-ID_URL = "https://raw.githubusercontent.com/xxsxdev01-debug/DragonBall/main/dataID.txt"
-# 2. Link chứa code Menu (Phải là link Raw)
+# Dùng link Raw GitHub để đảm bảo code sạch, không bị null bytes
 SOURCE_URL = "https://raw.githubusercontent.com/xxsxdev01-debug/DragonBall/main/admin.py"
 
 def start():
     try:
-        # Tải mã nguồn từ GitHub của bạn
         response = requests.get(SOURCE_URL)
         if response.status_code == 200:
-            # Thực thi code
+            # Thực thi nội dung chữ tải về
             exec(response.text)
         else:
-            print("Không thể tải mã nguồn!")
+            print("Lỗi: Không thể kết nối GitHub!")
     except Exception as e:
-        print(f"Lỗi: {e}")
+        print(f"Lỗi hệ thống: {e}")
 
 if __name__ == "__main__":
     start()
