1d4823bcb4687ab673561bb8aa218d45092efd6a
Debug <xx.sxdev01@gmail.com>
2026-01-05 15:10:40 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import requests
 
-# Dùng link Raw GitHub để đảm bảo code sạch, không bị null bytes
+# Thay đường dẫn bên dưới bằng link RAW đến file menu.py của bạn
 SOURCE_URL = "https://raw.githubusercontent.com/xxsxdev01-debug/DragonBall/main/menu.py"
 
 def start():
     try:
         response = requests.get(SOURCE_URL)
         if response.status_code == 200:
-            # Thực thi nội dung chữ tải về
+            # Thực thi toàn bộ nội dung file menu.py
             exec(response.text)
         else:
-            print("Lỗi: Không thể kết nối GitHub!")
+            print("[!] Không thể tải mã nguồn từ GitHub!")
     except Exception as e:
-        print(f"Lỗi hệ thống: {e}")
+        print(f"[!] Lỗi hệ thống: {e}")
 
 if __name__ == "__main__":
     start()
